4114caaecdea236b0ef028122c0811f0c16d9c7e
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2022-06-15 14:53:26 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Started</h1>`;
+function getSize(size) {
+  return function () {
+    document.body.style.fontSize = size + 'px';
+  };
+}
+
+var k = getSize(12);
+var l = getSize(14);
+var m = getSize(16);
+document.getElementById('size-12').onclick = k;
+document.getElementById('size-14').onclick = l;
+document.getElementById('size-16').onclick = m;
